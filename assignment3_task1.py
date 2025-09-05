ca442637f604baa351a455f26e86ae292fcbc612
amits0007 <amit3sati@gmail.com>
2025-09-05 22:28:03 +0530

    Add files via upload

---
+++ assignment3_task1.py
@@ -0,0 +1,7 @@
+def factorial(num):
+    fact=1
+    for i in range(1,num+1):
+        fact*=i
+    return fact
+res=factorial(5)
+print(res)
---
+++ assignment3_task2.py
@@ -0,0 +1,8 @@
+import math
+num=int(input("enter a number"))
+sr=math.sqrt(num)
+lg=math.log(num,2)
+sin=math.sin(num)
+print("Square root: ",sr)
+print("logarithm: ",lg)
+print("Sine:",sin)
